4b1e274d8270f214201bafbcb52dc6cf3799040b
Ryan <coderyrh9236@gmail.com>
2024-05-25 02:34:58 +0800

    update

--- game.js
+++ game.js
@@ -5,10 +5,22 @@ CoreLib.outputCallback = s => console.log(s);
 JsLib.registerLib(vm);
 
 const code = `
+; ----- vector2 -----
 (def! new-v2 (fn* [x y] (new 'Vector2 x y)))
+(defmacro! v2-set (fn* [v prop value] \`(.= ~v ~prop value)))
+(defmacro! v2-get (fn* [v prop] \`((symbol (str '. ~prop)) ~v)))
+(def! v2-print (fn* [v] (println "(" (.x v) ", " (.y v) ")")))
+(def! v2-set-x (fn* [v value] (v2-set v 'x value)))
+(def! v2-set-y (fn* [v value] (v2-set v 'y value)))
+(def! v2-get-x (fn* [v] (v2-get v 'x)))
+(def! v2-get-y (fn* [v] (v2-get v 'y)))
+; --------------------
+
 (def! new-color (fn* [r g b a] (new 'Color r g b a)))
 
+; ----- draw api -----
 (def! draw-rect (fn* [pos size color angle] ($drawRect pos size color angle)))
+; --------------------
 
 (def! vec2-add-x (fn* [vec value] 
     (let* [x (.x vec)]
